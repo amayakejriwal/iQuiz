fe22a823a65c77179e22215159c6c0dd30c7ef66
amayakejriwal <amayak@uw.edu>
2022-05-05 13:41:35 -0700

    added settings alert

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func settingsTouchUpInside(_ sender: Any) {
+        // V1: Show the user an alert
+        let alert = UIAlertController(title: "Settings", message: "Settings go here.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK",
+                                      style: .default,
+                                      handler: { _ in
+          NSLog("\"OK\" pressed.")
+        }))
+
+        self.present(alert, animated: true, completion: {
+          NSLog("The completion handler fired")
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
